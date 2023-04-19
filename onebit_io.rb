c212989f3aeb32a9c00a8712c7c1b920de7ea75f
Osiris Mariano <oslimacontato@gmail.com>
2023-04-19 20:19:13 -0300

    Adiciona diretorio conteito_básico

--- onebit_io.rb
+++
@@ -1,9 +0,0 @@
-# saída de dado
-print 'Digite seu nome: '
-
-# Recebendo uma entrada do teclado
-name = gets.chomp
-
-# saída utilizando puts
-# use códigos ruby dentro de uma string com #{code}
-puts "Hello #{name}!"
