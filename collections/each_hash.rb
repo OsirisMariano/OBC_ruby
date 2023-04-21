8413aa88f4ef65768a4b3ee0f297b54103f594c7
Osiris Mariano <oslimacontato@gmail.com>
2023-04-20 22:26:08 -0300

    Adiciona arquivo each_hash.rb

---
+++ collections/each_hash.rb
@@ -0,0 +1,5 @@
+aulas = {'Aula1 ' => 'liberada', 'Aula 2 ' => 'liberada', 'Aula 3 ' => 'liberada' , 'Aula 4 ' => 'liberda', 'Aula 5 ' 'em breve'}
+
+aulas.each do |key, value|
+  puts "#{key} #{value}"
+end
