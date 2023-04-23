63d02d5ea20a31b1b09f8653671b97bda06f3973
Osiris Mariano <oslimacontato@gmail.com>
2023-04-22 21:08:20 -0300

    feature: Adiciona arquivo cachorro.rb(require_relative)

--- POOII/animal/app.rb
+++ POOII/animal/app.rb
@@ -1,4 +1,11 @@
-require_relative 'aminal'
+require_relative 'animal'
+require_relative 'cachorro'
 
+puts '--Animal--'
 animal = Animal.new
 animal.pular
+
+put '--Cachorro--'
+cachorro = Cachorro.new
+cachorro.pular
+cachorro.latir
